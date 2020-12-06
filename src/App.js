183ae8bf25e465c25cf14771ba00b3a84ae64813
taraiihor <63466237+taraiihor@users.noreply.github.com>
2020-12-07 01:37:18 +0200

    деплой

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
       number,
     };
     if (contacts.find(contact => contact.name === newContact.name)) {
-      alert(`${newContact.name} з таким імененем вже є.`);
+      alert(`${newContact.name} з таким ім’ям вже є.`);
       return;
     }
 
